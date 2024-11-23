511b38f9ed2615f32348dd3e5d24ba94f6226144
sala 105 <sala105@poliacademico.edu.loc>
2024-11-23 10:44:55 -0500

    mala

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,5 +2,5 @@ import { test, expect } from 'vitest';
 
 test('multiplication function works', () => {
     const multiplicacion = (a, b) => a * b;
-    expect(multiplicacion(2, 3)).toBe(6);
+    expect(multiplicacion(2, 3)).toBe(9);
 });
